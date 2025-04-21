a0649f7cb631847b8576cbf2f8e2d55e0f8f8dd3
ycngmn <niloymollick90@gmail.com>
2025-04-22 00:10:53 +0200

    fix: Margin inconsistency on some pages.

--- app/src/main/java/com/ycngmn/nobook/utils/styling/stickyTopNavbarScript.kt
+++ app/src/main/java/com/ycngmn/nobook/utils/styling/stickyTopNavbarScript.kt
@@ -1,66 +1,64 @@
 package com.ycngmn.nobook.utils.styling
 
-val stickyTopNavbarScript =
-    """
-    (function() {
-        var a = function() {
-            var h = document.querySelector('div[data-tti-phase="-1"][data-mcomponent="MContainer"][data-type="container"][data-focusable="true"].m');
-            if (h && h.querySelector('div[aria-label="Logo Facebook"]')) {
-                h.style.position = 'fixed';
-                h.style.top = '0';
-                h.style.left = '0';
-                h.style.width = '100%';
-                h.style.zIndex = '1000';
-                h.style.pointerEvents = 'auto';
-            }
-    
-            var t = document.querySelector('div[role="tablist"][data-tti-phase="-1"][data-type="container"][data-mcomponent="MContainer"].m');
-            if (t && t.querySelector('div[aria-label*="feed"]')) {
-                t.style.position = 'fixed';
-                t.style.top = (h && h.querySelector('div[aria-label="Logo Facebook"]')) ? '43px' : '';
-                t.style.left = '0';
-                t.style.width = '100%';
-                t.style.zIndex = '999';
-                t.style.pointerEvents = 'auto';
-            }
-    
-            var v = document.querySelector('div[data-type="vscroller"][data-is-pull-to-refresh-allowed="true"]');
-            if (v) {
-                var o = (h ? 43 : 0) + (t ? 50 : 0);
-                var c = v.querySelector(':scope > div:not(.pull-to-refresh-spinner-container)');
-                if (c) {
-                    c.style.marginTop = o + 'px';
-                } else {
-                    v.style.paddingTop = o + 'px';
-                }
-    
-                var s = v.querySelector('.pull-to-refresh-spinner-container');
-                if (s) {
-                    s.style.position = 'absolute';
-                    s.style.top = '0';
-                    s.style.left = '0';
-                    s.style.right = '0';
-                    s.style.zIndex = '1001';
-                    s.style.display = 'flex';
-                    s.style.justifyContent = 'center';
-                    s.style.alignItems = 'center';
-                }
-    
-                var p = v.querySelector('.pull-to-refresh-spinner');
-                if (p) {
-                    p.style.margin = '0 auto';
-                }
-            }
-    
-            document.body.style.paddingTop = '0';
-            document.body.style.marginTop = '0';
-            document.body.style.overflow = 'visible';
-            document.body.style.height = '100%';
-        };
-    
-        a();
-        setTimeout(a, 2000);
-        var o = new MutationObserver(a);
-        o.observe(document.body, { childList: true, subtree: true });
-    })();
-    """.trimIndent()
+val stickyTopNavbarScript = """
+(function() {
+    const applyStyles = () => {
+        const navbar = document.querySelector('div[data-tti-phase="-1"][data-mcomponent="MContainer"][data-type="container"][data-focusable="true"].m');
+        const tabbar = document.querySelector('div[role="tablist"][data-tti-phase="-1"][data-type="container"][data-mcomponent="MContainer"].m');
+        const scroller = document.querySelector('div[data-type="vscroller"][data-is-pull-to-refresh-allowed="true"]');
+
+        const hasLogo = navbar?.querySelector('div[aria-label="Logo Facebook"]');
+        const hasFeed = tabbar?.querySelector('div[aria-label*="feed"]');
+
+        if (hasLogo) Object.assign(navbar.style, {
+            position: 'fixed',
+            top: '0',
+            left: '0',
+            width: '100%',
+            zIndex: '1000',
+            pointerEvents: 'auto'
+        });
+
+        if (hasFeed) Object.assign(tabbar.style, {
+            position: 'fixed',
+            top: hasLogo ? '43px' : '',
+            left: '0',
+            width: '100%',
+            zIndex: '999',
+            pointerEvents: 'auto'
+        });
+
+        if (scroller) {
+            const offset = (hasLogo ? 43 : 0) + (hasFeed ? 50 : 0);
+            const scrollContent = scroller.querySelector(':scope > div:not(.pull-to-refresh-spinner-container)');
+            scrollContent ? scrollContent.style.marginTop = offset + 'px' : scroller.style.paddingTop = offset + 'px';
+
+            const spinnerContainer = scroller.querySelector('.pull-to-refresh-spinner-container');
+            if (spinnerContainer) Object.assign(spinnerContainer.style, {
+                position: 'absolute',
+                top: '0',
+                left: '0',
+                right: '0',
+                zIndex: '1001',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            });
+
+            const spinner = scroller.querySelector('.pull-to-refresh-spinner');
+            if (spinner) spinner.style.margin = '0 auto';
+        }
+
+        Object.assign(document.body.style, {
+            paddingTop: '0',
+            marginTop: '0',
+            overflow: 'visible',
+            height: '100%'
+        });
+    };
+
+    applyStyles();
+    setTimeout(applyStyles, 2000);
+    new MutationObserver(applyStyles).observe(document.body, { childList: true, subtree: true });
+})();
+""".trimIndent()
