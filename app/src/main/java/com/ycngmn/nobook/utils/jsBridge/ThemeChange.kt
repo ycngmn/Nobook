e9bc854442f69c9ed4c13941831889b53f211f9f
ycngmn <niloymollick90@gmail.com>
2025-04-27 16:24:54 +0200

    refactor: Removed unused line.

--- app/src/main/java/com/ycngmn/nobook/utils/jsBridge/ThemeChange.kt
+++ app/src/main/java/com/ycngmn/nobook/utils/jsBridge/ThemeChange.kt
@@ -1,7 +1,5 @@
 package com.ycngmn.nobook.utils.jsBridge
 
-import android.os.Handler
-import android.os.Looper
 import android.webkit.JavascriptInterface
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.graphics.Color
@@ -10,8 +8,6 @@ import androidx.core.graphics.toColorInt
 class ThemeChange(
     private val themeColor: MutableState<Color>
 ) {
-    private val mainHandler = Handler(Looper.getMainLooper())
-
     @JavascriptInterface
     fun onThemeColorChanged(newColor: String?) {
         if (newColor.isNullOrBlank()) return
