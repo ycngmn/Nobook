4474649b3c43d5af26e61f7cf53854851553bc77
ycngmn <niloymollick90@gmail.com>
2025-05-02 00:50:25 +0200

    UPDATE APP v0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ycngmn.nobook"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
