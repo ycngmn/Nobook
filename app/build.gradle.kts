cfd6614de695397abb3b9c636d6f540e56dcd5b5
ycngmn <niloymollick90@gmail.com>
2025-05-04 19:00:23 +0200

    APP UPDATE: v0.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ycngmn.nobook"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
