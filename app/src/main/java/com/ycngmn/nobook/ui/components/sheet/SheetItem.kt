bba01f97d9f561e715e986f0c89eef6aa356046f
ycngmn <niloymollick90@gmail.com>
2025-06-12 19:09:36 +0200

    ui: add checkbox to toggleable items.
    - Removed subtitles.

--- app/src/main/java/com/ycngmn/nobook/ui/components/sheet/SheetItem.kt
+++ app/src/main/java/com/ycngmn/nobook/ui/components/sheet/SheetItem.kt
@@ -7,8 +7,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,7 +23,6 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.ycngmn.nobook.ui.theme.FacebookBlue
 
 @Composable()
 fun SheetItem(
@@ -39,7 +43,7 @@ fun SheetItem(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp, vertical = 10.dp)
+                .padding(horizontal = 16.dp, vertical = 6.5.dp)
         ) {
             Image(
                 painter = painterResource(icon),
@@ -47,13 +51,11 @@ fun SheetItem(
                 modifier = Modifier.size(35.dp),
                 colorFilter =
                     if (iconColor != null) ColorFilter.tint(iconColor)
-                    else if (isActive)
-                        ColorFilter.tint(FacebookBlue)
                     else ColorFilter.tint(MaterialTheme.colorScheme.secondary)
             )
 
             Column(
-                modifier = Modifier.padding(start = 16.dp)
+                modifier = Modifier.padding(start = 12.dp).weight(1F)
             ) {
                 Text(
                     text = title,
@@ -62,16 +64,27 @@ fun SheetItem(
                     modifier = Modifier.padding(bottom = 4.dp)
                 )
 
-                if (subtitle.isNotEmpty()) {
-                    Text(
-                        text = subtitle,
-                        color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.8F),
-                        fontSize = 15.sp
-                    )
-                }
+            }
+
+            if (iconColor == null) {
+
+                Switch(
+                    checked = isActive,
+                    onCheckedChange = { onClick() },
+                    modifier = Modifier.padding(horizontal = 10.dp),
+                    thumbContent = if (isActive) {
+                        {
+                            Icon(
+                                imageVector = Icons.Filled.Check,
+                                contentDescription = null,
+                                modifier = Modifier.size(SwitchDefaults.IconSize),
+                            )
+                        }
+                    } else null
+                )
             }
         }
     }
 
-    HorizontalDivider(Modifier, color = Color.Gray, thickness = 0.2.dp)
+    HorizontalDivider(color = Color.Gray, thickness = 0.2.dp)
 }
