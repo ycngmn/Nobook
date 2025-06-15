3d2d9e4b6ddf4352632714dd4e37748cedaaef30
ycngmn <niloymollick90@gmail.com>
2025-06-15 15:39:45 +0200

    fix: improved gear icon

--- app/src/main/res/raw/scripts.js
+++ app/src/main/res/raw/scripts.js
@@ -142,18 +142,19 @@ observer.observe(document.body, { childList: true, subtree: true });
 
 // Nobook settings : floating button
 (() => {
+
   const btn = Object.assign(document.createElement('button'), {
-    innerHTML: '⚙️',
+    innerHTML: `
+    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill='#ffffff' viewBox="0 0 24 24">
+      <path fill-rule="evenodd" d="M9.586 2.586A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2v.089l.473.196.063-.063a2.002 2.002 0 0 1 2.828 0l1.414 1.414a2 2 0 0 1 0 2.827l-.063.064.196.473H20a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-.089l-.196.473.063.063a2.002 2.002 0 0 1 0 2.828l-1.414 1.414a2 2 0 0 1-2.828 0l-.063-.063-.473.196V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.089l-.473-.196-.063.063a2.002 2.002 0 0 1-2.828 0l-1.414-1.414a2 2 0 0 1 0-2.827l.063-.064L4.089 15H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h.09l.195-.473-.063-.063a2 2 0 0 1 0-2.828l1.414-1.414a2 2 0 0 1 2.827 0l.064.063L9 4.089V4a2 2 0 0 1 .586-1.414ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z" clip-rule="evenodd"/>
+    </svg>
+`,
     style: `
       position: fixed;
-      top: 4px;
+      top: 8px;
       right: 98px;
-      width: 36px;
-      height: 36px;
       background: transparent;
-      box-shadow: 0.5px 0.5px 0.5px rgba(0, 0, 0, 0.2);
       color: white;
-      font-size: 23px;
       font-weight: bold;
       border: none;
       border-radius: 50%;
