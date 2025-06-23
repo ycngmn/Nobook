a75145df8e0d99987fc11c4f4c7fd9482b666f8b
ycngmn <41749571+ycngmn@users.noreply.github.com>
2025-06-24 00:55:49 +0200

    release: v0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ycngmn.nobook"
         minSdk = 22
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.0.9"
+        versionCode = 10
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
