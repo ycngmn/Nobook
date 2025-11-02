fd1b888fc3f47ff61353c9a024aebe465aa1595b
ycngmn <41749571+ycngmn@users.noreply.github.com>
2025-11-02 09:06:27 +0100

    chore: clean unused resources

--- app/src/main/java/com/ycngmn/nobook/ui/theme/Color.kt
+++ app/src/main/java/com/ycngmn/nobook/ui/theme/Color.kt
@@ -2,8 +2,6 @@ package com.ycngmn.nobook.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val BrightGray = Color(0XFFF5F5F5)
-
 val GoogleDark = Color(0xFF121212)
 val FacebookBlue = Color(0xFF1877F2)
 val FacebookDark = Color(0XFF232425)
