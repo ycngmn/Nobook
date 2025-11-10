d9df5f7848efb7d0e79a3df428b82a089a128739
ycngmn <41749571+ycngmn@users.noreply.github.com>
2025-11-10 17:31:31 +0100

    fix(ui): replace splash screen logo

--- app/src/main/java/com/ycngmn/nobook/ui/screens/SplashLoading.kt
+++ app/src/main/java/com/ycngmn/nobook/ui/screens/SplashLoading.kt
@@ -5,20 +5,24 @@ import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -36,11 +40,18 @@ fun SplashLoading(progress: Float) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(R.drawable.logo_round),
-            contentDescription = null,
-            modifier = Modifier.size(80.dp)
-        )
+        Box(
+            Modifier.size(68.dp).shadow(12.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .background(Color(0XFF09547A))
+        ) {
+            Icon(
+                painter = painterResource(R.mipmap.ic_launcher_foreground),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
 
         Spacer(Modifier.height(16.dp))
 
